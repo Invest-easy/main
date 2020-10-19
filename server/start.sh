fc3d6c28c61e7d62be6381ffbeaa4a6ca90b93a7
HDEROCHE <hugo.deroche@gmail.com>
2020-10-19 16:14:35 +0200

    Executable! Starting/installing the server

--- server/start.sh
+++ server/start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 PATH_TO_FILE=$(pwd)
+echo $PATH_TO_FILE
 cd PATH_TO_FILE
 # Checks for dependencies, if not found, it installs them
 npm install
-
+echo ''
 echo 'Packages installed/found'
 
 # Starts the server
